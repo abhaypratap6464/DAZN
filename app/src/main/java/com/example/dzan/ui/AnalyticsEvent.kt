0285d6370c14200320abda387d09adcdf53cb22c
Abhay Pratap <abhaypratap6464@gmail.com>
2023-04-20 03:26:36 +0530

    Code Optimized

---
+++ app/src/main/java/com/example/dzan/ui/AnalyticsEvent.kt
@@ -0,0 +1,9 @@
+package com.example.dzan.ui
+
+object AnalyticsEvent {
+
+    const val PAUSE_BTN_CLICKED = "pause_btn_clicked"
+    const val PLAY_BTN_CLICKED = "pause_btn_clicked"
+    const val FORWARD_BTN_CLICKED = "forward_btn_clicked"
+    const val BACKWARD_BTN_CLICKED = "backward_btn_clicked"
+}
--- app/src/main/java/com/example/dzan/ui/MainActivity.kt
+++ app/src/main/java/com/example/dzan/ui/MainActivity.kt
@@ -3,27 +3,75 @@ package com.example.dzan.ui
 import android.os.Bundle
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import com.example.dzan.R
 import com.example.dzan.databinding.ActivityMainBinding
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.source.dash.DashMediaSource
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
 import com.google.firebase.analytics.FirebaseAnalytics
 
-class MainActivity : AppCompatActivity(), Player.Listener {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var firebaseAnalytics: FirebaseAnalytics
     private var exoPlayer: ExoPlayer? = null
     private var playbackPosition = 0L
     private var playWhenReady = true
+    private lateinit var pauseBtn : ImageButton
+    private lateinit var playBtn : ImageButton
+    private lateinit var ffwdBtn : ImageButton
+    private lateinit var rewBtn : ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         firebaseAnalytics = FirebaseAnalytics.getInstance(this)
         setView()
+        initView()
         preparePlayer()
+        initClickListeners()
+    }
+
+    private fun initView() {
+        pauseBtn=findViewById(com.google.android.exoplayer2.R.id.exo_pause)
+        playBtn=findViewById(com.google.android.exoplayer2.R.id.exo_play)
+        ffwdBtn=findViewById(com.google.android.exoplayer2.R.id.exo_ffwd)
+        rewBtn=findViewById(com.google.android.exoplayer2.R.id.exo_rew)
+        binding.tvPauseCount.text = getString(R.string.pause_count,PAUSE_COUNT)
+        binding.tvBackwardCount.text = getString(R.string.rewbtn_count, REWIND_COUNT)
+        binding.tvForwardCount.text = getString(R.string.pause_count, FORWARD_COUNT)
+    }
+
+    private fun initClickListeners() {
+        pauseBtn.setOnClickListener {
+            firebaseAnalytics.logEvent(AnalyticsEvent.PAUSE_BTN_CLICKED,null)
+            PAUSE_COUNT +=1
+            pauseBtn.isEnabled = false
+            playBtn.isEnabled = true
+            binding.tvPauseCount.text = getString(R.string.pause_count,PAUSE_COUNT)
+            exoPlayer?.pause()
+        }
+
+        playBtn.setOnClickListener {
+            pauseBtn.isEnabled = true
+            playBtn.isEnabled = false
+            firebaseAnalytics.logEvent(AnalyticsEvent.PLAY_BTN_CLICKED,null)
+            exoPlayer?.play()
+        }
+
+        ffwdBtn.setOnClickListener {
+            FORWARD_COUNT +=1
+            binding.tvForwardCount.text = getString(R.string.ffwdbtn_count,FORWARD_COUNT)
+            firebaseAnalytics.logEvent(AnalyticsEvent.FORWARD_BTN_CLICKED,null)
+            exoPlayer?.seekForward()
+        }
+
+        rewBtn.setOnClickListener {
+            REWIND_COUNT +=1
+            binding.tvBackwardCount.text = getString(R.string.rewbtn_count, REWIND_COUNT)
+            firebaseAnalytics.logEvent(AnalyticsEvent.BACKWARD_BTN_CLICKED,null)
+            exoPlayer?.seekBack()
+        }
     }
 
     private fun setView() {
@@ -46,18 +94,6 @@ class MainActivity : AppCompatActivity(), Player.Listener {
             playWhenReady = playWhenReady
             prepare()
         }
-        exoPlayer?.addListener(this)
-
-        findViewById<ImageButton>(com.google.android.exoplayer2.R.id.exo_pause).setOnClickListener {
-            firebaseAnalytics.logEvent("PauseBtn",null)
-            exoPlayer?.pause()
-        }
-
-        findViewById<ImageButton>(com.google.android.exoplayer2.R.id.exo_play).setOnClickListener {
-            firebaseAnalytics.logEvent("PlayBtn",null)
-            exoPlayer?.play()
-        }
-
 
     }
 
@@ -84,8 +120,13 @@ class MainActivity : AppCompatActivity(), Player.Listener {
         super.onDestroy()
         releasePlayer()
     }
+
+
     companion object {
         const val URL = "https://storage.googleapis.com/wvmedia/clear/vp9/tears/tears_uhd.mpd"
+        var PAUSE_COUNT:Int = 0
+        var REWIND_COUNT = 0
+        var FORWARD_COUNT =0
     }
 }
 
